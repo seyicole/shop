2ade887114b202e3ecc11ce83ee2bc401f1cddb4
sey cole <sey cole>
2019-02-20 11:18:05 +0100

    added env var

--- backend-api/models/app.js
+++ backend-api/models/app.js
@@ -1,9 +1,8 @@
 let mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://seyi:s3y1c0l3@ds253889.mlab.com:53889/items', { useNewUrlParser: true });
-//mongodb://seyi:s3y1c0l3@ds253889.mlab.com:53889/items
+mongoose.connect('process.env.DB', { useNewUrlParser: true });
+//console.log(process.env.DB)
 mongoose.Promise = Promise;
 
 module.exports.Item = require('./items');
 
-//mongoose.connect('mongodb://localhost/items-api', { useNewUrlParser: true });
