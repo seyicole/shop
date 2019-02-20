a10d9b3d0138df049a122b9eddee18eced0bf5ce
sey cole <sey cole>
2019-02-20 11:33:30 +0100

    conf env var

--- backend-api/models/app.js
+++ backend-api/models/app.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('process.env.DB');
-//console.log(process.env.DB)
+mongoose.connect(process.env.DB || 'mongodb://localhost/items-api', { useNewUrlParser: true });
+console.log(process.env.DB)
 mongoose.Promise = Promise;
 
 module.exports.Item = require('./items');
