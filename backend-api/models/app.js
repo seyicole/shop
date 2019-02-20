e229cf2d667eb46ce4ca967b522e754588ccb09f
sey cole <sey cole>
2019-02-20 01:44:13 +0100

    added mongolab

--- backend-api/models/app.js
+++ backend-api/models/app.js
@@ -1,7 +1,9 @@
 let mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/items-api', { useNewUrlParser: true });
-
+mongoose.connect('mongodb://seyi:s3y1c0l3@ds253889.mlab.com:53889/items', { useNewUrlParser: true });
+//mongodb://seyi:s3y1c0l3@ds253889.mlab.com:53889/items
 mongoose.Promise = Promise;
 
 module.exports.Item = require('./items');
+
+//mongoose.connect('mongodb://localhost/items-api', { useNewUrlParser: true });
