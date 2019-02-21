d4ce47f8a57249582a1aa320570336ad4b2a8ead
Seyi <seyicole01@gmail.com>
2019-02-21 14:41:58 +0100

    deleted priva

--- private.js
+++
@@ -1,3 +0,0 @@
-//mongoose.connect('mongodb://seyi:s3y1c0l3@ds253889.mlab.com:53889/items', { useNewUrlParser: true });
-//mongoose.connect('mongodb://localhost/items-api', { useNewUrlParser: true });
-//console.log(process.env.DB)
